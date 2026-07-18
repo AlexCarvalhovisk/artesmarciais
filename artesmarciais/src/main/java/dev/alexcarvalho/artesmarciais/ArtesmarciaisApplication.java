685161cb66c83ede11cfc5a67db3ab33acab05a0
Alex <adscarvalho@restinga.ifrs.edu.br>
2026-07-18 15:35:51 -0300

    Primeiro commit

---
+++ artesmarciais/src/main/java/dev/alexcarvalho/artesmarciais/ArtesmarciaisApplication.java
@@ -0,0 +1,13 @@
+package dev.alexcarvalho.artesmarciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesmarciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesmarciaisApplication.class, args);
+	}
+
+}
---
+++ artesmarciais/src/test/java/dev/alexcarvalho/artesmarciais/ArtesmarciaisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexcarvalho.artesmarciais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtesmarciaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
