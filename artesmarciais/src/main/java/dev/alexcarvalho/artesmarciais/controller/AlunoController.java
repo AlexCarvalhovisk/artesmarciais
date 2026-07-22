f521cb2c810c8e23f36513f6df0d1c0758b82a60
Alex <adscarvalho@restinga.ifrs.edu.br>
2026-07-21 22:51:04 -0300

    Criei os testes iniciais para validações.

---
+++ artesmarciais/src/main/java/dev/alexcarvalho/artesmarciais/controller/AlunoController.java
@@ -0,0 +1,50 @@
+package dev.alexcarvalho.artesmarciais.controller;
+
+import dev.alexcarvalho.artesmarciais.dto.AlunoRequest;
+import dev.alexcarvalho.artesmarciais.dto.AlunoResponse;
+import dev.alexcarvalho.artesmarciais.service.AlunoService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    //Contrutor da classe
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    //Rotas do CRUD
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AlunoResponse cadastrar(@RequestBody AlunoRequest alunoRequest) {
+        return alunoService.cadastrar(alunoRequest);
+    }
+
+    @GetMapping
+    public Page<AlunoResponse> listar(Pageable pageable) {
+        return alunoService.listar(pageable);
+    }
+
+    //Sempre que uso interpolação como no caso abaixo do ID, tenho que usar a anotação PathVariable dentrp do metodo.
+    @GetMapping("/{id}")
+    public AlunoResponse buscarPorId(@PathVariable Long id) {
+        return alunoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AlunoResponse atualizar(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest) {
+        return alunoService.atualizar(id, alunoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id) {
+        alunoService.excluir(id);
+    }
+}
